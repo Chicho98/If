e806523ac3d9e6edcfd756dcb5fd61c8b477317b
Chicho98 <leecordeiro99@gmail.com>
2023-09-11 20:03:17 -0300

    Se agrega proyecto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("edad pls");
+int edad = Convert.ToInt32(Console.ReadLine());
+if
+    (edad >= 100)
+    Console.WriteLine("tas re viejo!");
+else if
+     (edad > 18)
+    Console.WriteLine("entraste wachen");
+
+else if 
+     (edad <= 0)
+     Console.WriteLine("no naciste");
+
+         
+
+
